5b491ca6c8c58bbaf81eb350a83c3399e090ba6e
Alexandre Giraud <alx_665@hotmail.fr>
2013-02-04 14:59:45 +0100

    Test listView

--- gen/univ/amu/applicationfamille/R.java
+++ gen/univ/amu/applicationfamille/R.java
@@ -17,13 +17,14 @@ public final class R {
         public static final int boutonTest=0x7f070008;
         public static final int container=0x7f070007;
         public static final int email=0x7f070003;
+        public static final int listView1=0x7f070009;
+        public static final int liste_course=0x7f07000a;
         public static final int login=0x7f070005;
         public static final int login_form=0x7f070002;
         public static final int login_status=0x7f070000;
         public static final int login_status_message=0x7f070001;
-        public static final int menu_forgot_password=0x7f07000b;
-        public static final int menu_settings=0x7f07000a;
-        public static final int mylist=0x7f070009;
+        public static final int menu_forgot_password=0x7f07000c;
+        public static final int menu_settings=0x7f07000b;
         public static final int password=0x7f070004;
         public static final int sign_in_button=0x7f070006;
     }
--- src/univ/amu/applicationfamille/ListeCourseFragment.java
+++ src/univ/amu/applicationfamille/ListeCourseFragment.java
@@ -10,26 +10,31 @@ import android.widget.ListView;
 
 public class ListeCourseFragment extends Fragment {
 
-	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);		
-
-		ListView listView = (ListView) getActivity().findViewById(R.id.mylist);
+		super.onCreate(savedInstanceState);
+		ListView liste = (ListView) getActivity().findViewById(R.id.liste_course);
 		String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
-		  "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
-		  "Linux", "OS/2" };
+				  "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+				  "Linux", "OS/2" };
 
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(),
-		  android.R.layout.simple_list_item_1, android.R.id.text1, values);
+				// Define a new Adapter
+				// First parameter - Context
+				// Second parameter - Layout for the row
+				// Third parameter - ID of the TextView to which the data is written
+				// Forth - the Array of data
+
+				ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+				  android.R.layout.simple_list_item_1, android.R.id.text1, values);
 
 
-		// Assign adapter to ListView
-		listView.setAdapter(adapter);
+				// Assign adapter to ListView
+				liste.setAdapter(adapter);
+		
 	}
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fragment_liste_courses, container, false);
+		return inflater.inflate(R.layout.fragment_repas, container, false);
 	}
 }
