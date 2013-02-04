44be811354893768aa31e5785c52fb3a6b03ed7a
florian <fberthereau@gmail.com>
2013-02-04 14:59:27 +0100

    c

--- gen/univ/amu/applicationfamille/R.java
+++ gen/univ/amu/applicationfamille/R.java
@@ -14,21 +14,22 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int boutonTest=0x7f070000;
+        public static final int boutonTest=0x7f070009;
+        public static final int calendrier=0x7f070000;
         public static final int container=0x7f070008;
-        public static final int description=0x7f07000b;
+        public static final int description=0x7f07000c;
         public static final int email=0x7f070004;
-        public static final int img=0x7f070009;
-        public static final int listviewperso=0x7f07000c;
+        public static final int img=0x7f07000a;
+        public static final int listviewperso=0x7f07000d;
         public static final int login=0x7f070006;
         public static final int login_form=0x7f070003;
         public static final int login_status=0x7f070001;
         public static final int login_status_message=0x7f070002;
-        public static final int menu_forgot_password=0x7f07000e;
-        public static final int menu_settings=0x7f07000d;
+        public static final int menu_forgot_password=0x7f07000f;
+        public static final int menu_settings=0x7f07000e;
         public static final int password=0x7f070005;
         public static final int sign_in_button=0x7f070007;
-        public static final int titre=0x7f07000a;
+        public static final int titre=0x7f07000b;
     }
     public static final class layout {
         public static final int activity_agenda=0x7f030000;
--- src/univ/amu/applicationfamille/AgendaFragment.java
+++ src/univ/amu/applicationfamille/AgendaFragment.java
@@ -1,19 +1,37 @@
 package univ.amu.applicationfamille;
 
+import java.security.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+
+import android.app.Activity;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 
 public class AgendaFragment extends Fragment {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		//Date d = new Date();
+		//CalendarView calendarAffichage = (CalendarView) findViewById(R.id.calendrier);
+		//Calendar calendrier = Calendar.getInstance();
+		//calendarAffichage.setAdapter();
 		
+		//CalendarContact c = CalendarContract.;
+		//calendar.setShowWeekNumber(true);
+		//calendar.setVerticalFadingEdgeEnabled(true);
 	}
 	
+	private View findViewById(int calendrier) {
+		return getActivity().findViewById(calendrier);
+	}
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.activity_agenda, container, false);
