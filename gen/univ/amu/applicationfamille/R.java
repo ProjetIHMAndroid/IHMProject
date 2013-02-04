2a0d6208ccf77b9a08ba240ad5b691cc13310aac
Alexandre Giraud <alx_665@hotmail.fr>
2013-02-04 14:00:39 +0100

    GROS COMMIT 2.0

--- gen/univ/amu/applicationfamille/R.java
+++ gen/univ/amu/applicationfamille/R.java
@@ -14,30 +14,26 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int boutonTest=0x7f070000;
-        public static final int container=0x7f070008;
-        public static final int description=0x7f07000b;
-        public static final int email=0x7f070004;
-        public static final int img=0x7f070009;
-        public static final int listviewperso=0x7f07000c;
-        public static final int login=0x7f070006;
-        public static final int login_form=0x7f070003;
-        public static final int login_status=0x7f070001;
-        public static final int login_status_message=0x7f070002;
-        public static final int menu_forgot_password=0x7f07000e;
-        public static final int menu_settings=0x7f07000d;
-        public static final int password=0x7f070005;
-        public static final int sign_in_button=0x7f070007;
-        public static final int titre=0x7f07000a;
+        public static final int boutonTest=0x7f070008;
+        public static final int container=0x7f070007;
+        public static final int email=0x7f070003;
+        public static final int login=0x7f070005;
+        public static final int login_form=0x7f070002;
+        public static final int login_status=0x7f070000;
+        public static final int login_status_message=0x7f070001;
+        public static final int menu_forgot_password=0x7f07000b;
+        public static final int menu_settings=0x7f07000a;
+        public static final int mylist=0x7f070009;
+        public static final int password=0x7f070004;
+        public static final int sign_in_button=0x7f070006;
     }
     public static final class layout {
-        public static final int activity_agenda=0x7f030000;
-        public static final int activity_liste_courses=0x7f030001;
-        public static final int activity_login=0x7f030002;
-        public static final int activity_main=0x7f030003;
-        public static final int activity_repas=0x7f030004;
-        public static final int display_item=0x7f030005;
-        public static final int list_fruit=0x7f030006;
+        public static final int activity_login=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int fragment_agenda=0x7f030002;
+        public static final int fragment_liste_courses=0x7f030003;
+        public static final int fragment_repas=0x7f030004;
+        public static final int liste_course=0x7f030005;
     }
     public static final class menu {
         public static final int activity_agenda=0x7f060000;
--- src/univ/amu/applicationfamille/ListeCourseFragment.java
+++ src/univ/amu/applicationfamille/ListeCourseFragment.java
@@ -1,83 +1,35 @@
 package univ.amu.applicationfamille;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
 
 public class ListeCourseFragment extends Fragment {
 
-	private ListView maListViewPerso;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);		
-		
-        maListViewPerso = (ListView) getActivity().findViewById(R.id.listviewperso);
- 
-        ArrayList<HashMap<String, String>> listItem = new ArrayList<HashMap<String, String>>();
- 
-        HashMap<String, String> map;
- 
-        map = new HashMap<String, String>();
-        map.put("titre", "Word");
-        map.put("description", "Editeur de texte");
-        //map.put("img", String.valueOf(R.drawable.word));
-        listItem.add(map);
- 
- 
-        map = new HashMap<String, String>();
-        map.put("titre", "Excel");
-        map.put("description", "Tableur");
-        //map.put("img", String.valueOf(R.drawable.excel));
-        listItem.add(map);
- 
-        map = new HashMap<String, String>();
-        map.put("titre", "Power Point");
-        map.put("description", "Logiciel de présentation");
-        //map.put("img", String.valueOf(R.drawable.powerpoint));
-        listItem.add(map);
- 
-        map = new HashMap<String, String>();
-        map.put("titre", "Outlook");
-        map.put("description", "Client de courrier électronique");
-        //map.put("img", String.valueOf(R.drawable.outlook));
-        listItem.add(map);
- 
-        SimpleAdapter mSchedule = new SimpleAdapter (getActivity().getBaseContext(), listItem, R.layout.display_item,
-               new String[] {"img", "titre", "description"}, new int[] {R.id.img, R.id.titre, R.id.description});
- 
-        maListViewPerso.setAdapter(mSchedule);
-        
-        /*maListViewPerso.setOnItemClickListener(new OnItemClickListener() {
-			@Override
-        	@SuppressWarnings("unchecked")
-         	public void onItemClick(AdapterView<?> a, View v, int position, long id) {
-				//on récupère la HashMap contenant les infos de notre item (titre, description, img)
-        		HashMap<String, String> map = (HashMap<String, String>) maListViewPerso.getItemAtPosition(position);
-        		//on créer une boite de dialogue
-        		AlertDialog.Builder adb = new AlertDialog.Builder(Tutoriel5_Android.this);
-        		//on attribut un titre à notre boite de dialogue
-        		adb.setTitle("Sélection Item");
-        		//on insère un message à notre boite de dialogue, et ici on affiche le titre de l'item cliqué
-        		adb.setMessage("Votre choix : "+map.get("titre"));
-        		//on indique que l'on veut le bouton ok à notre boite de dialogue
-        		adb.setPositiveButton("Ok", null);
-        		//on affiche la boite de dialogue
-        		adb.show();
-        	}
-         });*/
- 
+
+		ListView listView = (ListView) getActivity().findViewById(R.id.mylist);
+		String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+		  "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+		  "Linux", "OS/2" };
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(),
+		  android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+
+		// Assign adapter to ListView
+		listView.setAdapter(adapter);
 	}
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.activity_liste_courses, container, false);
+		return inflater.inflate(R.layout.fragment_liste_courses, container, false);
 	}
 }
